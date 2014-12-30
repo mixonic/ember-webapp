79e228582ea2ed8acbfb1f997ac7f8eed011fb84
murali <mmrath@gmail.com>
2014-12-30 21:54:12 +1100

    List

--- app/router.js
+++ app/router.js
@@ -8,9 +8,11 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource("admin", { path: "/admin" });
 
-  this.resource("admin.user", { path: "/admin/user" });
-  this.resource("admin.user.new", { path: "/admin/user/new" });
-  this.resource("admin.user.edit", { path: "/admin/user/:id" });
+  this.resource("admin.user",{path: '/admin/user'}, function() {
+    this.route('new');
+    this.route('edit', {path: '/:user_id/edit'});
+    this.route('list', {path: '/list'});
+  });
 
   this.resource("admin.role", { path: "/admin/role" });
   this.resource("admin.group", { path: "/admin/group" });
